45b1bdab2085798f6b2890c003acbee886ba9674
Jeremie Ges <bonjour@gesjeremie.fr>
2015-09-28 12:47:07 +0300

    hero component

---
+++ app/views/documentation/hero.blade.php
@@ -0,0 +1,22 @@
+@section('content')
+  <h1>Hero</h1>
+
+  <div class="example__section">
+
+    <div class="parent">
+      <div class="grid-12">
+        <div class="hero" style="background-image: url('https://images.unsplash.com/photo-1423683249427-8ca22bd873e0?q=80&fm=jpg&s=4c9cedceff75d717c39802725e9e8dfd')">
+          <div class="hero__overlay"></div>
+          <div class="hero__container">
+            <div class="hero__title">Hammer</div>
+            <div class="hero__headline">Kick-ass your front end development.</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://gist.github.com/GesJeremie/da802dade1748be775f0.js"></script>
+
+  </div>
+
+@endsection
--- app/views/layouts/documentation.blade.php
+++ app/views/layouts/documentation.blade.php
@@ -36,6 +36,7 @@
           <li class="@if ($page == 'alert') --active @endif"><a href="{{ url('documentation/alert') }}">Alert</a></li>
           <li class="@if ($page == 'button') --active @endif"><a href="{{ url('documentation/button') }}">Button</a></li>
           <li class="@if ($page == 'form') --active @endif"><a href="{{ url('documentation/form') }}">Form</a></li>
+          <li class="@if ($page == 'hero') --active @endif"><a href="{{ url('documentation/hero') }}">Hero</a></li>
           <li class="@if ($page == 'tooltip') --active @endif"><a href="{{ url('documentation/tooltip') }}">Tooltip</a></li>
         </ul>
       </div>
