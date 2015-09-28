7ea2c9517acba6d3774b51686be309c2dbf2e1eb
Jeremie Ges <bonjour@gesjeremie.fr>
2015-09-28 11:13:44 +0300

    form gist

--- app/views/documentation/form.blade.php
+++ app/views/documentation/form.blade.php
@@ -4,20 +4,24 @@
   <div class="example__section">
     <h2>Simple Form</h2>
 
-    <div class="grid-12">
-      <form class="form">
-        <label>Email</label>
-        <input type="text" name="email" />
+    <div class="parent">
+      <div class="grid-12">
+        <form class="form">
+          <label>Email</label>
+          <input type="text" name="email" />
 
-        <label>Name</label>
-        <input type="text" name="name" placeholder="Put your real name" />
+          <label>Name</label>
+          <input type="text" name="name" placeholder="Put your real name" />
 
-        <label>Description</label>
-        <textarea></textarea>
+          <label>Description</label>
+          <textarea></textarea>
 
-        <a class="button">Create User</a>
-      </form>
+          <a class="button">Create User</a>
+        </form>
+      </div>
 
     </div>
+
+    <script src="https://gist.github.com/GesJeremie/ff72107d64e25b36f855.js"></script>
   </div>
 @endsection
